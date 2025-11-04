2b9d8440bb61fbcf99620765f940b46bf35333f4
nashif770 <a.nashif7@gmail.com>
2025-11-04 16:24:11 +0600

    bangla

--- app/shared/Footer.jsx
+++ app/shared/Footer.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="w-full py-4 text-center text-gray-600 space-y-1">
-      <div>&copy; {new Date().getFullYear()} Al Furkan Girls Madrasa</div>
+      <div>&copy; {new Date().getFullYear()}আল ফুরকান ইসলামিয়া বালিকা মাদ্রাসা</div>
       <div className="text-sm text-gray-500">
         Built by <Link href="#" className="hover:text-emerald-600 font-medium">Ahmed Nashif</Link>
       </div>
